4112d9b7b0907defdcc76fd88b7ca84fbb0adac9
Mike <mike.chen@joocatech.com>
2026-05-11 00:46:13 +0800

    Initial Commit

---
+++ CaptureFlow/CaptureFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureFlowApp.swift
+//  CaptureFlow
+//
+//  Created by Mike Chen on 2026/5/11.
+//
+
+import SwiftUI
+
+@main
+struct CaptureFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptureFlow
+//
+//  Created by Mike Chen on 2026/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
